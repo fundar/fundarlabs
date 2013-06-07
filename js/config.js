19733b2a725e02136fb007ba2078c6070866e3ac
Carlos Hugo Gonzalez Castell (Fundar) <carlos@fundar.org.mx>
2013-06-07 13:14:15 -0500

    footer

--- js/config.js
+++ js/config.js
@@ -30,3 +30,22 @@ jQuery(function() {
 		detach: false
 	});
 });
+
+jQuery(function() {
+	// Links
+		var	_bh = jQuery('body, html'),
+		_window = jQuery(window),
+		_nav = jQuery('#nav');
+	
+		jQuery('a').click(function(e) {
+			var t = jQuery(this), h = t.attr('href'), article;
+
+			if (h.charAt(0) == '#' && h.length > 1 && (article = jQuery('article#' + h.substring(1))).length > 0)
+			{
+				var pos = Math.max(article.parent().offset().top - _nav.height() + 15, 0);
+				e.preventDefault();
+				_bh.animate({ scrollTop: pos }, 'slow', 'swing');
+			}
+		});
+
+});
